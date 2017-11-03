7e2a7026a7696ad9680706b0d57e183e47cf63bd
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-11-03 21:57:46 +0530

    Initial commit with empty response from server

---
+++ app/src/main/java/com/enpassio/infinitescroolingrecyclerview/MainActivity.java
@@ -0,0 +1,110 @@
+package com.enpassio.infinitescroolingrecyclerview;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GetItems getItems = new GetItems();
+        getItems.execute();
+    }
+
+    private class GetItems extends AsyncTask<String, Void, String> {
+        ProgressDialog dialog;
+        HttpURLConnection conn;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage("Loading, please wait...");
+            dialog.setTitle("Connecting server");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = "", jsonresponse = "";
+            BufferedReader bufferedReader = null;
+            JSONObject json = null;
+            JSONObject jsonObject = null;
+            URL url = null;
+            try {
+                Log.v("my_tag", "try runs");
+                url = new URL(params[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Authorization", "Token "+ "fe63a7b37e04515a4cba77d2960526a84d1a56da");
+                int responseCode = conn.getResponseCode();
+                System.out.println("responsecode--"+responseCode);
+                Log.e("my_tag", "responsecode --" + responseCode);
+                Log.e("my_tag", "url is--" +conn.getURL());
+                if (responseCode == HttpsURLConnection.HTTP_CREATED) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    //Log.d("Output",br.toString());
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                        Log.d("output lines", line);
+                    }
+                    json = new JSONObject(response);
+                    //Get Values from JSONobject
+                    // System.out.println("success=" + json.get("success"));
+
+                    jsonresponse = "success";
+
+                } else {
+                    InputStreamReader inputStreamReader = new InputStreamReader(conn.getErrorStream());
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        response += line;
+                        Log.d("output lines", line);
+                    }
+                    // Log.i("response", response);
+                    // json = new JSONObject(response);
+                    // jsonresponse = json.getString("error");
+                    //System.out.println("error=" + json.get("error"));
+                    //succes = json.getString("success");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return jsonresponse;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.v("my_tag", "result is: " + result);
+            if (dialog != null && dialog.isShowing()) {
+                dialog.dismiss();
+            }
+        }
+    }
+}
