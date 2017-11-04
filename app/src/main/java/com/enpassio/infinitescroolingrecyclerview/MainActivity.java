35f07736056328ce4db40bc48abfaed0d5049f08
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-11-04 16:31:12 +0530

    Data shown on RecyclerView-UI

--- app/src/main/java/com/enpassio/infinitescroolingrecyclerview/MainActivity.java
+++ app/src/main/java/com/enpassio/infinitescroolingrecyclerview/MainActivity.java
@@ -1,12 +1,11 @@
 package com.enpassio.infinitescroolingrecyclerview;
 
-import android.icu.text.DateFormat;
-import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -18,11 +17,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    String next_url;
+    RecyclerView mPopularMovieRecyclerView;
+    LinearLayoutManager layoutManagerPopularMoviesPoster;
+
+    RecyclerViewAdapter mPopularMoviesAdapter;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -30,55 +35,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getItems("http://ec2-35-154-135-19.ap-south-1.compute.amazonaws.com:8001/api/reminders/");
+
+
+        mPopularMovieRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        layoutManagerPopularMoviesPoster = new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.VERTICAL, false);
+            /* Optionally customize the position you want to default scroll to */
+        layoutManagerPopularMoviesPoster.scrollToPosition(0);
+            /* Attach layout manager to the RecyclerView */
+        mPopularMovieRecyclerView.setLayoutManager(layoutManagerPopularMoviesPoster);
+        mPopularMoviesAdapter = new RecyclerViewAdapter(MainActivity.this, new ArrayList<String>());
+        mPopularMovieRecyclerView.setAdapter(mPopularMoviesAdapter);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public  void getItems(String url) {
-        System.out.println("getItemcalled");
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(dateFormat.format(new Date()));
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,url,
+    public void getItems(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            Log.v("my_tag", "json response is: "+response);
-                            JSONObject jsonObject=new JSONObject(response);
+                            JSONObject jsonObject = new JSONObject(response);
+                            next_url = jsonObject.getString("next");
 
-                            Log.v("my_tag", "json jsonObject is: "+jsonObject);
                             JSONArray jsonArray = jsonObject.getJSONArray("results");
 
-                            Log.v("my_tag", "json array is: "+jsonArray);
-                        }
-                        catch (JSONException e) {
+                            ArrayList<String> data = new ArrayList<>();
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject object = jsonArray.getJSONObject(i);
+
+                                String id = object.getString("id");
+                                data.add(id);
+                                //Log.v("my_tag", "id is: " + id);
+                            }
+
+                            updateUI(data);
+
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
-                },new Response.ErrorListener() {
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 // Log.i("response--", String.valueOf(error));
             }
-        }){
+        }) {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
 
                 return params;
             }
+
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String,String>header=new HashMap<>();
+                HashMap<String, String> header = new HashMap<>();
                 header.put("Content-Type", "application/json; charset=utf-8");
 
-                 header.put("Authorization","Token fe63a7b37e04515a4cba77d2960526a84d1a56da");
-                //header.put("Authorization","Token "+ "fe63a7b37e04515a4cba77d2960526a84d1a56da");
-
-                // header.put("Content-Type", "application/x-www-form-urlencoded");
+                header.put("Authorization", "Token fe63a7b37e04515a4cba77d2960526a84d1a56da");
 
                 return header;
             }
-        } ;
+        };
         MyVolleySingleton.getInstance(MainActivity.this).getRequestQueue().add(stringRequest);
+    }
+
+    private void updateUI(ArrayList<String> data) {
+        mPopularMoviesAdapter.setMovieData(data);
     }
 }
---
+++ app/src/main/java/com/enpassio/infinitescroolingrecyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,99 @@
+package com.enpassio.infinitescroolingrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ABHISHEK RAJ on 11/4/2017.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    /* Store a member variable for the popularMovies */
+    private static ArrayList<String> mPopularMovie;
+    /* Store the context for easy access */
+    private Context mContext;
+
+    /* Pass in the popularMovies array into the constructor */
+    public RecyclerViewAdapter(Context context, ArrayList<String> movies) {
+        mPopularMovie = movies;
+        mContext = context;
+    }
+
+    /* Easy access to the context object in the recyclerview */
+    private Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        /* Inflate the custom layout */
+        View moviesView = inflater.inflate(R.layout.item_recyclerview, parent, false);
+
+        /* Return a new holder instance */
+        ViewHolder viewHolder = new RecyclerViewAdapter.ViewHolder(context, moviesView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapter.ViewHolder viewHolder, int position) {
+        /* Get the data model based on position */
+        String currentMovie = mPopularMovie.get(position);
+        Log.i("my_tag", "currentId is: " + currentMovie);
+        /*
+        Set item views based on your views and data model
+         */
+        viewHolder.movieTitleTextView.setText(currentMovie);
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.i("my_tag", "getItemCount is: " + mPopularMovie.size());
+        return mPopularMovie.size();
+    }
+
+    public void setMovieData(ArrayList<String> movieData) {
+        Log.v("my_tag", "setMovieData called with size: " + movieData.size());
+        mPopularMovie = movieData;
+        notifyDataSetChanged();
+    }
+
+    /*
+     Provide a direct reference to each of the views within a data item
+     Used to cache the views within the item layout for fast access
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        /*
+        Your holder should contain a member variable
+        for any view that will be set as you render a row
+        */
+        public final TextView movieTitleTextView;
+
+        private Context context;
+
+        /*
+        We also create a constructor that accepts the entire item row
+        and does the view lookups to find each subview
+        */
+        public ViewHolder(Context context, View itemView) {
+            /*
+            Stores the itemView in a public final member variable that can be used
+            to access the context from any ViewHolder instance.
+            */
+            super(itemView);
+            movieTitleTextView = (TextView) itemView.findViewById(R.id.idTextView);
+
+            this.context = context;
+        }
+    }
+}
